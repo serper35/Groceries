5075d22f6ccd80abe1e7a4cb0fd42e6e77d249b8
serper35 <serper.wolf@gmail.com>
2023-12-04 19:51:28 +0300

    test

---
+++ Groceries/src/test/java/com/serper/Groceries/GroceriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serper.Groceries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
