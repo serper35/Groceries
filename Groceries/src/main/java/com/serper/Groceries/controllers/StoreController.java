6e1c90217ba0ace1f03dcfe55205346ce73f9500
serper35 <serper.wolf@gmail.com>
2023-12-09 12:21:47 +0300

    test

--- Groceries/src/main/java/com/serper/Groceries/controllers/StoreController.java
+++ Groceries/src/main/java/com/serper/Groceries/controllers/StoreController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/store/order")
+@RequestMapping("/order")
 @RestController
 public class StoreController {
 
